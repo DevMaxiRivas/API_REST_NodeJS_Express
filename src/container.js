b662c6d6b930f6558bceab0c66e5a30ff9f686aa
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-08 13:54:57 -0300

    fix(dependences): The file that manages dependency injection has been updated

--- src/container.js
+++ src/container.js
@@ -3,14 +3,17 @@ import pool from './config/database.js'
 import UserRepository from './repositories/userRepository.js'
 import UserService from './services/userService.js'
 import UserController from './controllers/userController.js'
+import AuthController from './controllers/authController.js'
+import AuthService from './services/authService.js'
 
-// 1. Infraestructura
 const userRepository = new UserRepository(pool)
 
-// 2. Servicios (reciben repositories)
 const userService = new UserService(userRepository)
 
-// 3. Controllers (reciben services)
+const authService = new AuthService(userRepository)
+
 const userController = new UserController(userService)
 
-export default userController
+const authController = new AuthController(authService)
+
+export { userController, authController }
