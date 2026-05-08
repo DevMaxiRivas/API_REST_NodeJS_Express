bfcfa1266c8482c64a21eb6cb9360ffdda4e922e
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-08 07:50:21 -0300

    feat(dtos): Implement DTOs before moving on to the service

---
+++ src/dtos/userCreateDTO.js
@@ -0,0 +1,9 @@
+export default class userCreateDTO {
+    constructor({ name, username, email, password }) {
+        this.username = username
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+}
+
--- src/dtos/userUpdateDTO.js
+++ src/dtos/userUpdateDTO.js
@@ -14,6 +14,5 @@ export default class userUpdateDTO {
 
         return filledFields
     }
-
-
 }
+
