53cc2322019b3b30ef54760399067d8b1ab98968
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-08 13:50:29 -0300

    fix(request): rule have been added to createUserRequest

--- src/requests/user/createUserRequest.js
+++ src/requests/user/createUserRequest.js
@@ -3,7 +3,8 @@ import { body } from 'express-validator'
 const createUserRequest = [
     body('name').isString().withMessage('name is required and must be a string'),
     body('email').isEmail().withMessage('email is required and most be a email'),
-    body('password').isString().withMessage('password is required and must be a string')
+    body('password').isString().withMessage('password is required and must be a string'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long')
 ]
 
 export default createUserRequest
