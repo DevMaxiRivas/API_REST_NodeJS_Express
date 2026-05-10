7b42a2635839aa7dd2864ea8f6dfd5c9342e3240
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-09 22:52:43 -0300

    feat(request): registerRequest rules have been implemented

---
+++ src/requests/auth/registerRequest.js
@@ -0,0 +1,10 @@
+import { body } from 'express-validator'
+
+const registerRequest = [
+    body('name').isString().withMessage('name is required and must be a string'),
+    body('email').isEmail().withMessage('email is required and most be a email'),
+    body('password').isString().withMessage('password is required and must be a string'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 chars long')
+]
+
+export default registerRequest
