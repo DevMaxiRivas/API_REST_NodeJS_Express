e1db899ad9d66b7c20659add96b5752c882aa58e
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-09 22:53:31 -0300

    fix(dto): name field have been added in userResponse

--- src/dtos/userResponseDTO.js
+++ src/dtos/userResponseDTO.js
@@ -3,6 +3,7 @@ import { maskEmail } from '../utils/format/maskEmail.js'
 export class UserResponseDTO {
     constructor(user) {
         this.id = user.id
+        this.name = user.name
         this.username = user.username
         this.email = maskEmail(user.email)
     }
@@ -10,6 +11,7 @@ export class UserResponseDTO {
     getJsonResponse() {
         return {
             id: this.id,
+            name: this.name,
             username: this.username,
             email: this.email
         }
