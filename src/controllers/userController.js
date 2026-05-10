b4c2f71349891978826fafce432155ca8fc417ba
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-09 22:55:05 -0300

    createApiResponse have been implemented in userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,49 +1,47 @@
-import { getFullUrl } from '../lib/getFullUrl.js';
-import { ApiError, BadRequestError, NotFoundError, ValidationError } from '../utils/errors.js';
-import { ApiResponse, CreatedResponse, SuccessResponse } from '../utils/apiResponse.js';
-import { response } from 'express';
-import { UserResponseDTO } from '../dtos/userResponseDTO.js';
+import { getFullUrl } from '../lib/getFullUrl.js'
+import { ApiError, BadRequestError, NotFoundError, ValidationError } from '../utils/errors.js'
+import { createApiResponse } from '../utils/apiResponse.js'
+import { response } from 'express'
 
+import { UserResponseDTO } from '../dtos/userResponseDTO.js'
 import userUpdateDTO from '../dtos/userUpdateDTO.js'
-import { body } from 'express-validator';
-import userCreateDTO from '../dtos/userCreateDTO.js';
+import userCreateDTO from '../dtos/userCreateDTO.js'
 
 export default class userController {
     constructor(userService) {
-        this.userService = userService; // inyectado
+        this.userService = userService // inyectado
     }
 
     getAll = async (req, res, next) => {
         try {
-            const users = await this.userService.getAll();
-            const response = new SuccessResponse(users.map(user => (new UserResponseDTO(user)).getJsonResponse()));
-            res.status(parseInt(response.statusCode)).json(response.getJsonResponse());
+            const users = await this.userService.getAll()
+            const response = createApiResponse(200, users.map(user => (new UserResponseDTO(user)).getJsonResponse()))
+            res.status(response.statusCode).json(response.getJsonResponse())
 
         } catch (err) {
-            next(err);
+            next(err)
         }
-    };
+    }
 
     getById = async (req, res, next) => {
         try {
-            const user = await this.userService.getById(req.params.id);
-            const response = new SuccessResponse((new UserResponseDTO(user)).getJsonResponse());
-            res.status(parseInt(response.statusCode)).json(response.getJsonResponse());
+            const user = await this.userService.getById(req.params.id)
+            const response = createApiResponse(200, (new UserResponseDTO(user)).getJsonResponse())
+            res.status(response.statusCode).json(response.getJsonResponse())
         } catch (err) {
-            next(err);
+            next(err)
         }
-    };
+    }
 
     create = async (req, res, next) => {
         try {
-            const user = await this.userService.create(new userCreateDTO(req.body));
-            const response = new CreatedResponse((new UserResponseDTO(user)).getJsonResponse());
-            res.status(parseInt(response.statusCode)).json(response.getJsonResponse());
+            const user = await this.userService.create(new userCreateDTO(req.body))
+            const response = createApiResponse(201, (new UserResponseDTO(user)).getJsonResponse())
+            res.status(response.statusCode).json(response.getJsonResponse())
         } catch (err) {
-            next(err);
-
+            next(err)
         }
-    };
+    }
 
     update = async (req, res, next) => {
         try {
@@ -55,12 +53,30 @@ export default class userController {
             }
 
             const user = await this.userService.update(req.params.id, data)
-            const response = new SuccessResponse((new UserResponseDTO(user)).getJsonResponse());
-            res.status(parseInt(response.statusCode)).json(response.getJsonResponse());
+            const response = createApiResponse(200, (new UserResponseDTO(user)).getJsonResponse())
+            res.status(response.statusCode).json(response.getJsonResponse())
         } catch (err) {
-            next(err);
+            next(err)
 
         }
-    };
+    }
+
+    updateAuthUser = async (req, res, next) => {
+        try {
+            const { id, username } = req.session.user
+            const dto = new userUpdateDTO(req.body)
+            const data = dto.getFilledFields()
+
+            if (Object.keys(data).length === 0) {
+                throw new ValidationError('Validation Error', 'No fields to update', 'body')
+            }
+
+            const user = await this.userService.update(id, data)
+            const response = createApiResponse(200, (new UserResponseDTO(user)).getJsonResponse())
+            res.status(response.statusCode).json(response.getJsonResponse())
+        } catch (err) {
+            next(err)
+        }
+    }
 
 }
