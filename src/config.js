f444d8c0894590940a91f0017b8063377a2ff16a
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-09 22:40:33 -0300

    fix(config): enviroment variables have been declared in the config.js file

--- src/config.js
+++ src/config.js
@@ -5,4 +5,13 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_USER = process.env.DB_USER || 'postgres'
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres'
 export const DB_NAME = process.env.DB_NAME || 'postgres'
+export const DB_PORT = process.env.DB_PORT || 5432
+
+export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY || 'secret'
+export const SECRET_JWT_REFRESH_KEY = process.env.SECRET_JWT_REFRESH_KEY || 'refreshSecret'
+export const SERVER_URL = process.env.SERVER_URL
+
+export const ENCRYPT_SALT = parseInt(process.env.ENCRYPT_SALT) || 10
+
+export const NODE_ENV = process.env.NODE_ENV
 
