55053fbac5fe8a4c974134759888614b7d652101
DevMaxiRivas <maximilianorivas01@gmail.com>
2026-05-11 19:32:54 -0300

    fix(function):_Unnecessary try-catch blocks were removed

--- src/lib/validateJWTToken.js
+++ src/lib/validateJWTToken.js
@@ -46,11 +46,6 @@ import { createApiError } from '../utils/errors.js'
  */
 export function validateJWTToken(token = null, secretKey, source = 'headers') {
     if (!token) throw createApiError(401, 'Unauthorized Error', { title: 'Authentication failed. Token not found.', source: source })
-
-    try {
-        jwt.verify(token, secretKey)
-        return true
-    } catch (err) {
-        throw createApiError(401, 'Unauthorized Error', { title: 'Authentication failed. Invalid token.', source: source })
-    }
+    jwt.verify(token, secretKey)
+    return true
 }
